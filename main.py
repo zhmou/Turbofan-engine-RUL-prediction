fix loss computation bug

main.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from torch import optim as optim
 from turbofandataset import Turbofandataset
 from torch.utils.data import DataLoader
@@ -13,17 +14,28 @@ if __name__ == '__main__':
     testset = Turbofandataset(mode='test',
                               dataset='./datasets/CMAPSSData/test_FD004_normed.txt',
                               rul_result='./datasets/CMAPSSData/RUL_FD004.txt')
+    test_loader = DataLoader(dataset=testset, batch_size=64, shuffle=False, num_workers=2)
+    print('dataset load successfully!')
 
-    test_loader = DataLoader(dataset=testset, batch_size=248, shuffle=False, num_workers=2)
-    model = Model()
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    best_score_list = []
+    best_RMSE_list = []
+    for iteration in range(2):
+        print('---Iteration: {}---'.format(iteration + 1))
+        model = Model()
+        optimizer = optim.Adam(model.parameters(), lr=1e-3)
+        epochs = 32
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        trainer = Trainer(model=model,
+                          model_optimizer=optimizer,
+                          print_every=50,
+                          epochs=epochs,
+                          device=device,
+                          prefix='FD004')
+        best_score, best_RMSE = trainer.train(train_loader, test_loader, iteration)
+        best_score_list.append(best_score)
+        best_RMSE_list.append(best_RMSE)
 
-    epochs = 32
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    trainer = Trainer(model=model,
-                      model_optimizer=optimizer,
-                      print_every=50,
-                      epochs=epochs,
-                      device=device,
-                      prefix='FD004')
-    trainer.train(train_loader, test_loader)
+    best_score_list = np.array(best_score_list)
+    best_RMSE_list = np.array(best_RMSE_list)
+    result = np.concatenate((best_score_list, best_RMSE_list)).reshape(2, 2)
+    np.savetxt('./{}_result.txt'.format(trainer.prefix), result, fmt='%.4f')

model.py
@@ -10,14 +10,14 @@ class Model(nn.Module):
         self.linear = nn.Sequential(
             nn.Linear(in_features=1500, out_features=50),
             nn.ReLU(inplace=True),
-            nn.Dropout(p=0.1),
+            nn.Dropout(p=0.2),
             nn.Linear(in_features=50, out_features=10),
             nn.ReLU(inplace=True)
         )
         self.handcrafted = nn.Sequential(
             nn.Linear(in_features=34, out_features=10),
             nn.ReLU(inplace=True),
-            nn.Dropout(p=0.1)
+            nn.Dropout(p=0.2)
         )
 
         self.output = nn.Sequential(
@@ -50,5 +50,6 @@ class Attention3dBlock(nn.Module):
         x = inputs.permute(0, 2, 1)
         x = self.linear(x)
         x_probs = x.permute(0, 2, 1)
+        # print(torch.sum(x_probs.item()))
         output = x_probs * inputs
         return output

train.py
@@ -36,22 +36,33 @@ class Trainer:
                                                                         ))
                 running_loss = 0
 
-    def train(self, train_loader, test_loader):
+    def train(self, train_loader, test_loader, iteration):
         for epoch in range(self.epochs):
             print('Epoch: {}'.format(epoch + 1))
             self.model.train()
             self.train_single_epoch(train_loader)
-            self.test(test_loader)
-            self.save_checkpoints(epoch + 1)
+            current_score, current_RMSE = self.test(test_loader)
+            if epoch == 0:
+                best_score = current_score
+                best_RMSE = current_RMSE
+            else:
+                if current_score < best_score:
+                    best_score = current_score
+                    self.save_checkpoints(iteration + 1, epoch + 1, 'best_score')
+                if current_RMSE < best_RMSE:
+                    best_RMSE = current_RMSE
+                    self.save_checkpoints(iteration + 1, epoch + 1, 'best_RMSE')
+        return float(best_score), float(best_RMSE)
 
-    def save_checkpoints(self, epoch):
+    def save_checkpoints(self, iteration, epoch, which_type):
         state = {
+            'iter': iteration,
             'epoch': epoch,
             'state_dict': self.model.state_dict(),
             'optim_dict': self.model_optimizer.state_dict()
         }
-        torch.save(state, './checkpoints/' + self.prefix + '_' + str(epoch) + '.pth.tar')
-        print('checkpoints saved successfully!')
+        torch.save(state, './checkpoints/{}_iteration{}_{}.pth.tar'.format(self.prefix, iteration, which_type))
+        print('{}_checkpoints saved successfully!'.format(which_type))
 
     @staticmethod
     def score(y_true, y_pred):
@@ -79,6 +90,7 @@ class Trainer:
                     don't change the multiplier(130 or 150) here unless you changed the value of max_rul in turbofandataset.py
                 '''
                 score += self.score(labels * 150, predictions * 150)
-                loss += criterion(labels * 150, predictions * 150)
-
-        print('test result: score: {}, RMSE: {}'.format(score.item(), loss ** 0.5))
+                loss += criterion(labels * 150, predictions * 150) * len(labels)
+        loss = (loss / len(test_loader.dataset)) ** 0.5
+        print('test result: score: {}, RMSE: {}'.format(score.item(), loss))
+        return score.item(), loss

turbofandataset.py
@@ -61,9 +61,9 @@ class Turbofandataset(Dataset):
                     an interpolation operation will be performed
                 '''
                 if len(data_temp) < self.window_size:
-                    data = np.zeros((self.window_size, data_temp.shape[1]), dtype=np.float64)
+                    data = np.zeros((self.window_size, data_temp.shape[1]))
                     for j in range(data.shape[1]):
-                        x_old = np.linspace(0, len(data_temp)-1, len(data_temp))
+                        x_old = np.linspace(0, len(data_temp)-1, len(data_temp), dtype=np.float64)
                         params = np.polyfit(x_old, data_temp[:, j].flatten(), deg=1)
                         k = params[0]
                         b = params[1]
@@ -82,6 +82,11 @@ class Turbofandataset(Dataset):
         for i in range(len(self.x)):
             one_sample = self.x[i]
             self.mean_and_coef.append(self.fea_extract(one_sample))
+
+        mu = np.mean(self.mean_and_coef, axis=0)
+        sigma = np.std(self.mean_and_coef, axis=0)
+        eps = 1e-10
+        self.mean_and_coef = (self.mean_and_coef - mu) / (sigma + eps)
         self.mean_and_coef = np.array(self.mean_and_coef)
 
     @staticmethod